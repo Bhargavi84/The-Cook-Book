8d23c2a01f0be3ed53eebcaf4e24dfa0309fe7be
Bhargavi84 <bhargavi.parikshit@yahoo.com>
2021-07-09 11:07:57 +0000

    Add recipe form input fields and styling

--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,7 @@
   $(document).ready(function(){
       $('.sidenav').sidenav({edge:"right"});
       $('.collapsible').collapsible();
+      $('#textarea1').val('New Text');
+  M.textareaAutoResize($('#textarea1'));
     
   });
